152f3e53796b2a6425feed1ac8278a143b569fcf
Jessie A. Morris <jessie.morris@finicity.com>
2017-10-30 10:25:38 -0600

    Change the way we handle asyncs to be more interfacey-based

--- index.js
+++ index.js
@@ -1,58 +1,23 @@
-const co = require('co');
 const Layer = require('express/lib/router/layer');
 
 Object.defineProperty(Layer.prototype, "handle", {
   enumerable: true,
   get: function() { return this.__handle; },
   set: function(fn) {
-    if (isGenerator(fn)) {
-      fn = wrapGenerator(fn);
-    }
-
-    if (isAsync(fn)) {
-      fn = wrapAsync(fn);
-    }
+    fn = wrap(fn);
 
     this.__handle = fn;
   }
 });
 
-function isGenerator(fn) {
-  const type = Object.toString.call(fn.constructor);
-  return type.indexOf('GeneratorFunction') !== -1;
-}
+function wrap(fn) {
+  return (req, res, next) => {
+    const routePromise = fn(req, res, next);
 
-function isAsync(fn) {
-  const type = Object.toString.call(fn.constructor);
-  return type.indexOf('AsyncFunction') !== -1;
-}
-
-function wrapGenerator(original) {
-  const wrapped = co.wrap(original);
-  return function() {
-    const args = [].slice.call(arguments);
-    if(args.length === 4){
-      return original.apply(this, args)
+    if (routePromise.catch && typeof routePromise.catch === 'function') {
+      routePromise.catch(err => next(err));
     }
 
-    const res = args[1];
-    const next = args[2];
-
-    return wrapped.apply(this, args).then(() => {
-      !res.headersSent && next();
-    }).catch(err => next(err || {}));
-  };
-}
-
-function wrapAsync(fn) {
-  return function(){
-    const args = [].slice.call(arguments);
-    const val = fn.apply(this, args);
-
-    if(args.length !== 4) {
-      val.catch(err => args[2](err || {}));
-    }
-
-    return val;
+    return routePromise;
   }
-}
+};
